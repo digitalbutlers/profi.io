84179fbbdbdedfb731d67c92007978692286bc27
kirillsakun <kirillsakun03@gmail.com>
2022-04-22 11:33:13 +0400

    health-coaching-software.js refactored

---
+++ js/pages/coaches/health-coaching-software.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-undef
+initDropdowns({
+	faqQuestionSelector: '.faq-question',
+	hiddenBlocksSelector: '.faq-answer-wrapper',
+	itemSelector: '.faq-item',
+	activeClass: 'active',
+});
